576d1e44127a26e660bab4d6ce51769d8871a3c3
Graham Vickers <43250387+grahamvickers@users.noreply.github.com>
2022-01-26 09:17:46 -0500

    search works in backend

--- app/Http/Controllers/Api/ListingController.php
+++ app/Http/Controllers/Api/ListingController.php
@@ -6,26 +6,40 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Listing;
 
-
 class ListingController extends Controller
 {
     /**
      * Return all instances of the listing model
-     * 
+     *
      * @return Illuminate\Http\JsonResponse
-     * 
+     *
      */
 
-    public function index(){
-        $listings = Listing::all()->map->only([
-            'name',
-            'imgUri',
-            'address',
-            'price',
-            'profit',
-            'income',
-            'content'
-        ]);
+    public function index(Request $request){
+        if ($request->q) {
+            $listings = Listing::where('city', 'LIKE', "%{$request->q}%")
+                ->orWhere('name', 'LIKE', "%{$request->q}%")
+                ->get()
+                ->map->only([
+                'name',
+                'imgUri',
+                'address',
+                'price',
+                'profit',
+                'income',
+                'content'
+            ]);
+        } else {
+            $listings = Listing::all()->map->only([
+                'name',
+                'imgUri',
+                'address',
+                'price',
+                'profit',
+                'income',
+                'content'
+            ]);
+        }
 
         return response()->json($listings, 200);
     }
